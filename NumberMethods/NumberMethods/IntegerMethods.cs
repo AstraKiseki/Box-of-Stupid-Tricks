e426c3780c9380acd6f68b9ea20160d660fb3761
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-22 12:09:50 -0700

    Added Integer Methods class
    Only has Get Integer from User at the moment.

---
+++ NumberMethods/NumberMethods/IntegerMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberMethods
+{
+    public class IntegerMethods
+    {
+       // For when you need a number from the user.  Do note that if it's something as simple as a counter, it'll be just like x++; that's all.
+        static int GetIntegerFromUser(string prompt)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(prompt);
+                    string input = Console.ReadLine();
+                    int x = int.Parse(input);
+                    return x;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+            }
+        }
+    }
+}
