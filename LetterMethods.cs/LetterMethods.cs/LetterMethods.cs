0720577d8c1d2ed867a48d9524d02d8555a27b34
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-24 07:47:55 -0700

    added Letter Methods!  (Only has a mask string trick)

---
+++ LetterMethods.cs/LetterMethods.cs/LetterMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterMethods
+{
+    public class LetterMethods
+    {
+        static string QuoteAnswer = "This is for practice!";
+
+        // This trick was used in quote hangman and the inspiration for the whole repo.
+        static string maskString(string input)
+        {
+            string[] words = QuoteAnswer.Split(' ');
+            string DisplayWord = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                for (int j = 0; j < currentWord.Length; j++)
+                {
+                    DisplayWord += "-";
+                }
+                DisplayWord += " ";
+            }
+            DisplayWord = DisplayWord.TrimEnd();
+            return DisplayWord;
+        }
+    }
+}
