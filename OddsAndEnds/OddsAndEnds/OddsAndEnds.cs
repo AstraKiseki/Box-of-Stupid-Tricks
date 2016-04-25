8f12018c569acff140b5a5c0fea6860419579653
Rebecca Mooney <astrakiseki@gmail.com>
2016-04-25 07:04:47 -0700

    Close Method Added

---
+++ OddsAndEnds/OddsAndEnds/OddsAndEnds.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddsAndEnds
+{
+    public class OddsAndEnds
+    {
+        public static void Close()
+        {
+            Console.ReadKey();
+            Console.WriteLine("Would you like to run another calculation? (y/n)"); // for good practice
+            string userValue = Console.ReadLine();
+            userValue = userValue.ToLower();
+            if (userValue == "y")
+            {
+                Environment.Exit(0);
+            }
+            if (userValue == "n")
+            {
+                Console.WriteLine("Well we need something to do then!");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid answer.");
+            }
+        }
+    }
+}
