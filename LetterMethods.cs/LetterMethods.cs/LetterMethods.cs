34670c95f524c87333be3fa46b0bdf21dff83e85
Rebecca Mooney <astrakiseki@gmail.com>
2016-05-09 12:09:02 -0700

    Added the letter guess code I used for Quote Hangman

--- LetterMethods.cs/LetterMethods.cs/LetterMethods.cs
+++ LetterMethods.cs/LetterMethods.cs/LetterMethods.cs
@@ -28,5 +28,34 @@ namespace LetterMethods
             DisplayWord = DisplayWord.TrimEnd();
             return DisplayWord;
         }
+
+        public static bool IsThereA(string guessLetter) // Credit to DRapp for this
+        {
+            if (usedLetters.Contains(guessLetter) == false)
+            {
+                int maxlength = QuoteAnswer.Length;
+                bool anyMatch = false;
+                for (int i = 0; i < QuoteAnswer.Length; i++)
+                {
+                    if (QuoteAnswer.Substring(i, 1).Equals(guessLetter))
+                    {
+                        anyMatch = true;
+                        DisplayWord = DisplayWord.Substring(0, i) + guessLetter + DisplayWord.Substring(i + 1);
+                    }
+                }
+                usedLetters.Add(guessLetter);
+                return anyMatch;
+
+            }
+            if (usedLetters.Contains(guessLetter))
+            {
+                Console.WriteLine("Oh!  You have already used:");
+                usedLetters.ForEach(item => Console.Write(item + " "));
+                Console.WriteLine(" ");
+            }
+            return true;
+        }
+    }
+}
     }
 }
