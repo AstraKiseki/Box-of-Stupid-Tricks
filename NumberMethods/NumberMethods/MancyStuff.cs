305d3681596a0194d69a799974ec109646ed84e3
Rebecca Mooney <astrakiseki@gmail.com>
2016-05-17 06:23:55 -0700

    Plotted out Mancy stuff.

---
+++ NumberMethods/NumberMethods/MancyStuff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberMethods
+{
+    class MancyStuff
+    {
+        /*
+        Objective: http://www.d20pfsrd.com/feats/general-feats/arithmancy  Make a calculator for this.
+        Needs to take in a string, 
+        turn its letters into an array, 
+        turn them into numbers, 
+        add them all together, 
+        convert that into a string to see how many characters are in it,
+        if it is equal to 1, then you have the digital root.
+        If it isn't, convert that string back into an array
+        and repeat
+        */
+    }
+}
